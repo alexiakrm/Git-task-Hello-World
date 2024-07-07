1a3920cb5bb96db9170f9fcf9b00b29e8cfcb5fe
Alexia <alexiakrm.46@gmail.com>
2024-07-07 18:11:06 +0100

    Clean up unused variable occupation

--- helloWorld.py
+++ helloWorld.py
@@ -2,8 +2,5 @@
 name = input("Please enter your name:")
 print(name)
 
-# Issue 2 - set default occupation for user 
-occupation = "Software Engineer"
-
 # Issue 3 - Cappitalise user's name 
 name_caps = name.upper()
